760233f8838c45424891fa5082788c7f72fd9e11
kcopper8 <kcopper8@gmail.com>
2022-03-13 11:59:42 +0900

    add handling custom text logic #5

--- src/components/CustomizableTextToolView/index.tsx
+++ src/components/CustomizableTextToolView/index.tsx
@@ -1,6 +1,9 @@
+import { ChangeEventHandler, useState } from 'react';
+
 import { ArrowDownIcon, ArrowRightIcon } from '@chakra-ui/icons';
 import {
   Center,
+  Code,
   Container,
   FormControl,
   FormErrorMessage,
@@ -12,30 +15,90 @@ import {
   VStack,
 } from '@chakra-ui/react';
 
-import useTransformTool from '../../pages/useTransformTool';
-
 interface CustomizableTextToolViewProps {
   title: string;
 }
+
+function toTransformFunction(
+  functionBody: string
+): (inputText: string) => string {
+  const functionScriptText = `
+    (function customTransformText(inputText) {
+      ${functionBody}
+    });
+    `;
+  // eslint-disable-next-line no-eval
+  return eval(functionScriptText);
+}
 function CustomizableTextToolView({ title }: CustomizableTextToolViewProps) {
-  const { inputText, transformedText, error, onChangeText } = useTransformTool(
-    (s) => s
+  const [customTransformFunctionBody, setCustomTransformFunctionBody] =
+    useState('return inputText;');
+  const [functionBodyError, setFunctionBodyError] = useState<null | string>(
+    null
   );
+  const [inputText, setInputText] = useState('');
+  const [transformedText, setTransformedText] = useState('');
+
+  const [error, setError] = useState<Error | null>(null);
+
+  const handleChangeCustomTransformFunctionBody: ChangeEventHandler<
+    HTMLTextAreaElement
+  > = (event) => {
+    const newFunctionBody = event.target.value;
+    try {
+      setTransformedText(toTransformFunction(newFunctionBody)(inputText));
+      setFunctionBodyError(null);
+    } catch (e) {
+      setFunctionBodyError(String(e));
+    }
+    setCustomTransformFunctionBody(newFunctionBody);
+  };
+
+  const onChangeText: ChangeEventHandler<HTMLTextAreaElement> = (event) => {
+    const newInputText = event.target.value;
+    setInputText(newInputText);
+    try {
+      setError(null);
+      setTransformedText(
+        toTransformFunction(customTransformFunctionBody)(newInputText)
+      );
+    } catch (e) {
+      if (e instanceof Error) {
+        setError(e);
+      } else {
+        setError(new Error(String(e)));
+      }
+    }
+  };
 
   return (
     <Center pos="fixed" w="100%" h="100%">
       <VStack maxW="100%" width="100%">
         <Heading>{title}</Heading>
         <Container maxW="90%">
-          <FormControl>
+          <FormControl isInvalid={!!functionBodyError}>
             <FormLabel htmlFor="custom_logic">Custom Logic</FormLabel>
-            <FormHelperText>Enter Logic as a function</FormHelperText>
+            {functionBodyError && (
+              <FormErrorMessage>{functionBodyError}</FormErrorMessage>
+            )}
+            {!functionBodyError && (
+              <FormHelperText>Enter Logic as a function:</FormHelperText>
+            )}
+
+            <FormHelperText>
+              <Code>function(inputText) {'{'}</Code>
+            </FormHelperText>
             <Textarea
               id="custom_logic"
               placeholder="Enter Text"
               minH="10ex"
               resize="vertical"
+              value={customTransformFunctionBody}
+              onChange={handleChangeCustomTransformFunctionBody}
             />
+            <FormHelperText>
+              <Code>{'}'}</Code>
+            </FormHelperText>
           </FormControl>
         </Container>
         <ArrowDownIcon />
